05e3704f3e7e2387c9ba6a464222a7c3e170ee94
Dario <dario@resolute-datascience.com>
2021-01-27 21:35:53 +0000

    improved

--- stacks_and_queues/largest_rectangle.py
+++ stacks_and_queues/largest_rectangle.py
@@ -1,44 +1,56 @@
 histograms = [
     [1, 3, 2, 1, 2],
     [1, 3, 5, 3, 2, 2, 3, 3, 1],
-    [1, 3, 5, 3, 2, 2, 3, 3, 1, 0, 3, 6, 0, 3, 3]
+    [1, 3, 5, 3, 2, 2, 3, 3, 1, 0, 3, 6, 0, 3, 3],
+    [6320, 6020, 6098, 1332, 7263, 672, 9472, 2838, 3401, 9494],
+    [8979, 4570, 6436, 5083, 7780, 3269, 5400, 7579, 2324, 2116],
 ]
 
-solutions = [5, 14, 14]
+solutions = [5, 14, 14, 18060, 26152]
+
+
+"""
+areas = []
+start = []
+
+"""
 
 
 def largest_rectangle(histogram):
-    stack = []
-    pos = []
+
+    if len(histogram) == 0:
+        return 0, [0]
+
+    if len(histogram) == 1:
+        return histogram[0], histogram
+
     areas = []
-    max_area = -1
+    histogram.append(0)
+    start_coordinate = [0]
+    heights = [histogram[0]]
 
-    for i, elem in enumerate(histogram):
-        new_hist_start = i
-        while len(stack) > 0 and elem < stack[-1]:
-            histo_start = pos.pop()
-            height = stack.pop()
-            base = i - histo_start
-            area = height * base
-            areas.append(area)
-            max_area = max(max_area, area)
+    for position in range(1, len(histogram)):
+        elem = histogram[position]
 
-            new_hist_start = histo_start
+        if elem > heights[-1]:
+            start_coordinate.append(position)
+            heights.append(elem)
 
-        if len(stack) > 0 and elem > stack[-1] or len(stack) == 0:
-            stack.append(elem)
-            pos.append(new_hist_start)
+        elif elem < heights[-1]:
+            starts_at = position
+            while len(heights) > 0 and elem < heights[-1]:
+                height = heights[-1]
+                starts_at = start_coordinate[-1]
 
-    # Flush end of the stack
-    while len(stack) > 0:
-        histo_start = pos.pop()
-        height = stack.pop()
-        base = len(histogram) - histo_start
-        area = height * base
-        areas.append(area)
-        max_area = max(max_area, area)
+                del start_coordinate[-1]
+                del heights[-1]
 
-    return max_area, areas
+                areas.append(height * (position - starts_at))
+
+            start_coordinate.append(starts_at)
+            heights.append(elem)
+
+    return max(areas), areas
 
 
 for hist, sol in zip(histograms, solutions):
